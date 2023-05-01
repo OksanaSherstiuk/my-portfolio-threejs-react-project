2398187134e0c49c87b1f188698d0c222b9ddd0a
OksanaSherstiuk <moonka.moonka@gmail.com>
2023-05-01 20:19:12 +0200

    2/3 of tutorial, to be continued...

--- src/App.jsx
+++ src/App.jsx
@@ -26,8 +26,8 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
+          {/* <Contact />
+          <StarsCanvas /> */}
         </div>
       </div>
     </BrowserRouter>
